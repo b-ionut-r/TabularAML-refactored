c547ed2095a5e1a746c50b6b3497d8f46bd31236
= <=>
2026-03-30 19:11:50 +0300

    fix: change SocketIO async_mode from 'gevent' to 'threading' for improved compatibility

--- tabularaml/ui/feature_generator_server.py
+++ tabularaml/ui/feature_generator_server.py
@@ -35,7 +35,7 @@ app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024 * 1024  # 2GB max file size
 socketio = SocketIO(
     app,
     cors_allowed_origins="*",
-    async_mode='gevent',
+    async_mode='threading',
     ping_interval=20,
     ping_timeout=1800,
 )
